a8c6c3f08a4891cc42ec65ba9f8c3b0fbbbcaed6
geowarin@gmail.com <geowarin@gmail.com>
2018-07-01 04:13:19 +0200

    Aliases and functions

---
+++ .config/zsh/functions.zsh
@@ -0,0 +1,53 @@
+
+# Create a new directory and enter it
+function mkd() {
+	mkdir -p "$@" && cd "$_";
+}
+
+# Change working directory to the top-most Finder window location
+function cdf() { # short for `cdfinder`
+	cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')";
+}
+
+# Determine size of a file or total size of a directory
+function fs() {
+	if du -b /dev/null > /dev/null 2>&1; then
+		local arg=-sbh;
+	else
+		local arg=-sh;
+	fi
+	if [[ -n "$@" ]]; then
+		du $arg -- "$@";
+	else
+		du $arg .[^.]* ./*;
+	fi;
+}
+
+# Start an HTTP server from a directory, optionally specifying the port
+function server() {
+	local port="${1:-8000}";
+	sleep 1 && open "http://localhost:${port}/" &
+	# Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+	# And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+	python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port";
+}
+
+# Compare original and gzipped file size
+function gz() {
+	local origsize=$(wc -c < "$1");
+	local gzipsize=$(gzip -c "$1" | wc -c);
+	local ratio=$(echo "$gzipsize * 100 / $origsize" | bc -l);
+	printf "orig: %d bytes\n" "$origsize";
+	printf "gzip: %d bytes (%2.2f%%)\n" "$gzipsize" "$ratio";
+}
+
+# Shows real path of a binary, following symlinks
+function w() {
+	perl -MCwd -le 'print Cwd::abs_path(shift)' `which $@`
+}
+
+# Downloads a site completely with wget
+function dl-site() {
+	wget --mirror --convert-links --adjust-extension --page-requisites -U Mozilla --no-parent -e robots=off --wait=3 --random-wait --limit-rate=900k --no-check-certificate $@
+}
+
--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -2,19 +2,30 @@ export ZPLUG_HOME=/usr/local/opt/zplug
 
 source $ZPLUG_HOME/init.zsh
  
+# zgen oh-my-zsh plugins/github
+# zgen oh-my-zsh plugins/sudo
+# zgen oh-my-zsh plugins/command-not-found
+# zgen oh-my-zsh plugins/sublime
+# zgen oh-my-zsh plugins/npm
+# zgen oh-my-zsh themes/wedisagree
+
 zplug "rupa/z", use:z.sh
+
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-autosuggestions"
-
 zplug "zsh-users/zsh-history-substring-search"
-# Syntax highlighting needs to be loaded after compinit
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+
+zplug "plugins/sublime", from:oh-my-zsh
+zplug "lib/key-bindings", from:oh-my-zsh
+
+# Prompt
 zplug "sindresorhus/pure"
 
-zplug "zsh-users/zsh-syntax-highlighting"
+# Syntax highlighting needs to be loaded after compinit
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 export ZPLUG_LOADFILE="$XDG_CONFIG_HOME/zsh/zplug.zsh"
-if ! zplug check --verbose; then
+if ! zplug check; then
     zplug install
 fi
 
