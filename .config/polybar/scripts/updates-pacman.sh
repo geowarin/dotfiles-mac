c85045ae24b0c98d27df21739ddc3de8b5667715
Geoffroy Warin <code@geowarin.com>
2021-01-11 01:22:09 +0100

    polybar: audio and pacman updates

---
+++ .config/polybar/scripts/updates-pacman.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# from: https://github.com/polybar/polybar-scripts/blob/master/polybar-scripts/updates-pacman/updates-pacman.sh
+if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
+    updates=0
+fi
+
+if [ "$updates" -gt 0 ]; then
+    echo " $updates "
+else
+    echo ""
+fi
