b95e290c3748b210c0893e62e374dfe251bff8bb
Geoffroy Warin <code@geowarin.com>
2021-01-11 11:55:34 +0100

    polybar: tray (a bit unstable)

--- .config/polybar/start.sh
+++ .config/polybar/start.sh
@@ -2,6 +2,6 @@
 
 killall -q polybar
 
-#polybar main 2> /home/geowarin/.cache/poly-main.log & disown
-polybar primary & disown
+polybar primary 2> /home/geowarin/.cache/poly-main.log & disown
+#polybar primary & disown
 polybar secondary & disown
