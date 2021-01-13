d23d192d51cac63aa0c3c0de2c2403badc9cb801
Geoffroy Warin <code@geowarin.com>
2021-01-13 02:29:07 +0100

    Fix install script to handle sub directories

--- .bin/install.sh
+++ .bin/install.sh
@@ -2,13 +2,18 @@ git clone --bare git@github.com:geowarin/dotfiles.git $HOME/dotfiles
 function dotfiles {
    /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $@
 }
+function back {
+    mkdir -p $(dirname "$2")
+    mv $1 $2
+}
+
 dotfiles checkout
 if [ $? = 0 ]; then
-  echo "Checked out dotfiles.";
-  else
-    mkdir -p .dotfiles-backup
-    echo "Backing up pre-existing dotfiles.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    echo "Checked out dotfiles."
+else
+    echo "Backing up pre-existing dotfiles."
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} \
+        | xargs -I{} bash -c 'mkdir -p ".dotfiles-backup/{}" && mv "{}" ".dotfiles-backup/{}"'
 fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
